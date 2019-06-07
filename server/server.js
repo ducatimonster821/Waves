17097ad93fe4d473354455f8af9c005eef7f2079
vyacheslavsergin <vyacheslavsergin@gmail.com>
2019-06-07 21:00:58 +0500

    lesson79

--- server/server.js
+++ server/server.js
@@ -27,6 +27,36 @@ const {admin} = require('./middleware/admin');
 // PRODUCTS
 // --------------------------------------------
 
+// BY ARRIVAL
+// /articles?sortBy=createdAt&order=desc&limit=4
+
+// BY SELL
+// /articles?sortBy=sold&order=desc&limit=100&skip=5
+app.get('/api/product/articles', (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc';
+
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    console.log('order:', order); // desc
+    console.log('sortBy:', sortBy); // createdAt
+    console.log('limit:', limit); // 4
+
+    Product.find()
+        .populate('brand')
+        .populate('wood')
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, articles) => {
+            if (err) {
+                return res.status(400).send(err);
+            }
+            res.send(articles);
+        });
+});
+
+
 /// /api/product/article?id=HSHSHSKSK,JSJSJSJS,SDSDHHSHDS,JSJJSDJ&type=single
 app.get('/api/product/articles_by_id', (req, res) => {
     let type = req.query.type;
