acf0be47b16ca74fd15e5d6e7873505bfc3936dc
vyacheslavsergin <vyacheslavsergin@gmail.com>
2019-06-08 19:25:29 +0500

    lesson84

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 import Routes from './routes';
+import Reducer from './reducers';
 import './Resources/css/styles.css';
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
-, document.getElementById('root'));
+    <Provider
+        store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 // ReactDOM.render(<App/>, document.getElementById('root'));
 
