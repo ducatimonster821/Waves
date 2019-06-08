99a7777d5a3b3d8e06ff8f95776abc9c93a7e222
vyacheslavsergin <vyacheslavsergin@gmail.com>
2019-06-08 18:33:52 +0500

    lesson81

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from '../../App';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <App />
+                home
+            </div>
+        );
+    }
+}
+
+export default Home;
